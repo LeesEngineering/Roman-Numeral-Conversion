2cdd0203a37516ea5959add73067b9f7bfc6fc3c
Bruce A. Lee <brucelee774@hotmail.com>
2017-04-05 23:33:55 -0500

    "second commit"

--- test.rb
+++ test.rb
@@ -8,6 +8,10 @@ Roman_Numerals = {
   "I" => 1,
 }
 
+#Reverses the Roman Numerals and Arabic Numbers around in the Hash
+#To look like 1000 => "M" instead.
+Arabic_Numberals = Hash[Roman_Numerals.to_a.reverse.reverse]
+
 #Input Roman Numeral or a Number
 puts "-------------------------------------"
 print "Input a roman numeral number or a regular number for conversion: "
@@ -20,21 +24,40 @@ public
 def numeric?
   Float(self) != nil rescue false
 end
-puts "Is the input roman (String = false) or a number (Integer = true)? "
-puts input.numeric?
+#puts "Is the input roman (String = false) or a number (Integer = true)? "
+#puts input.numeric?
+true_false = input.numeric?
+#puts "-------------------------------------"
 
-puts "-------------------------------------"
-#Iterates through each hash (This is a test)
-Roman_Numerals.each do |value, roman|
-  #The "puts value and roman is a test to see the output"
-  puts value
-  puts roman
+#Looks for true or false from true_false variable. Then goes through the case to
+#convert a roman numeral or a number.
+
+case true_false
+when false
+#Converts a Roman Numeral to a Number
+  Roman_Numerals.each do |roman, value|
+  puts "#{roman}:#{value}"
+    #if roman == input
+      #puts value
+    #  puts "Answer: The Roman Numeral '#{input}' => #{value}."
+    #  break
+    #else
+    #  next
+    #end
+  end
+
+#Converts a Number to a Roman Numeral
+when true
+  Arabic_Numberals.each do |arabic, letter|
+    puts "#{letter}:#{arabic}"
+    puts "#{input}"
+    if input == arabic
+        puts "Answer: The Number '#{input}' => #{letter}"
+        break
+    else
+      puts "Why isn't this code working?"
+      next
+    end
+  end
 end
 puts "-------------------------------------"
-#End of test
-
-=begin
-def romanize
-
-end
-=end
